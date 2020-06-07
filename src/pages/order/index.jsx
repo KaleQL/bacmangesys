d83ae32cef0698eb08e3488cf16e7a2e9b392006
kale <1115931750@qq.com>
2020-06-07 17:30:46 +0800

    add

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Card, Button, Radio } from "antd";
+import "../ui.less";
 
 export default class Order extends React.Component {
   render() {
-    return <div>order</div>;
+    return (
+      <div style={{ width: "100%" }}>
+        <Card className="cardWrap"></Card>
+      </div>
+    );
   }
 }
